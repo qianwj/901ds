02b16c375a71d17eb8b0f24bae95b0014ab05fdc
elvis <qwjlu@sina.com>
2023-07-31 16:22:47 +0800

    add comment

--- leetcode/array/p80.cpp
+++ leetcode/array/p80.cpp
@@ -7,6 +7,12 @@
 #include "iostream"
 using namespace std;
 
+/**
+ * 删除排序数组中的重复项 II
+ *
+ * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+ * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+ */
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
